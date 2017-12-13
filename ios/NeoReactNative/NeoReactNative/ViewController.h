a0a5524b9a6eacdaf2c70fa1e1c8d92a5c690850
roxy_huang <sixin_huang@outlook.com>
2017-12-13 17:02:03 +0800

    init part of ios

---
+++ ios/NeoReactNative/NeoReactNative/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NeoReactNative
+//
+//  Created by Neo on 13/12/2017.
+//  Copyright © 2017 Neo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/NeoReactNative/NeoReactNative/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NeoReactNative
+//
+//  Created by Neo on 13/12/2017.
+//  Copyright © 2017 Neo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
