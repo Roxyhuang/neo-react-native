ddf25fa74937a5b7e96720a7545ebddc6c52cca7
roxy_huang <sixin_huang@outlook.com>
2017-12-29 18:15:05 +0800

    update react-native-router-flux

---
+++ src/routers/MainRouter.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import {
+  Scene,
+  Router,
+  Actions,
+  Reducer,
+  ActionConst,
+  Overlay,
+  Tabs,
+  Modal,
+  Drawer,
+  Stack,
+  Lightbox,
+} from 'react-native-router-flux';
+
+import HomeScreen from '../containers/HomeScreen';
+import IndexScreen from '../containers/IndexScreen';
+import ReduxScreen from '../containers/ReduxScreen';
+
+
+
+const prefix = Platform.OS === 'android' ? 'mychat://mychat/' : 'mychat://';
+
+const MainRouter = () => (
+    <Router>
+      <Tabs>
+        <Stack key="root">
+          <Scene key="Home" component={HomeScreen} title="Home" />
+          <Scene key="Index" component={IndexScreen} title="Index" />
+          <Scene key="Redux" component={ReduxScreen} title="Redux" />
+        </Stack>
+        <Stack key="root">
+          <Scene key="Home" component={HomeScreen} title="Home" />
+          <Scene key="Index" component={IndexScreen} title="Index" />
+          <Scene key="Redux" component={ReduxScreen} title="Redux" />
+        </Stack>
+      </Tabs>
+
+    </Router>
+);
+
+export default MainRouter;
--- src/views/App.js
+++ src/views/App.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as homeActions from '../core/actions/homeActions';
-import MainStackRouter from '../routers/MainStackRouter';
+import MainRouter from '../routers/MainRouter';
+// import MainStackRouter from '../routers/MainStackRouter';
+// import CodePush from 'react-native-code-push';
+// import { Container, Content, Text } from 'native-base';
+// import Modal from 'react-native-modalbox';
 
-const mapStateToProps = function(state) {
+const mapStateToProps = (state) => {
   const { home } = state;
   return {
     home,
@@ -16,25 +20,6 @@ const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators(
       { ...homeActions }, dispatch),
 });
-
-// import CodePush from 'react-native-code-push';
-// import { Container, Content, Text } from 'native-base';
-// import Modal from 'react-native-modalbox';
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     width: null,
-//     height: null,
-//   },
-//   modal: {
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   modal1: {
-//     height: 300,
-//   },
-// });
 
 class App extends Component {
   constructor(props) {
@@ -50,7 +35,7 @@ class App extends Component {
    // console.log(this.props.home.home);
   }
   render() {
-    return <MainStackRouter />;
+    return <MainRouter />;
   }
 }
 
